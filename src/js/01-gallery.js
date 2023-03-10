fba39f0df93490a8de9b755633fc68be019e79f7
AleksHor <horosh861@gmail.com>
2023-03-10 12:35:59 +0200

    correct task 8.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 
+import SimpleLightbox from 'simplelightbox';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
